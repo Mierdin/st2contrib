4e7eb984d9471b1e63ae683bf41b6309e3d254a6
Matt Oswalt <oswaltm@brocade.com>
2016-11-28 10:51:36 -0800

    Updated NAPALM sensor
    Signed-off-by: Matt Oswalt <oswaltm@brocade.com>

--- packs/napalm/sensors/napalm_bgpsensor.py
+++ packs/napalm/sensors/napalm_bgpsensor.py
@@ -1,9 +1,11 @@
-import sys
-import os
+from datetime import datetime
 
 from napalm import get_network_driver
 
 from st2reactor.sensor.base import PollingSensor
+
+BGP_PEER_INCREASE = 'napalm.BgpPeerIncrease'
+BGP_PEER_DECREASE = 'napalm.BgpPeerDecrease'
 
 
 class NapalmBGPSensor(PollingSensor):
@@ -19,13 +21,23 @@ class NapalmBGPSensor(PollingSensor):
         - indicates the interval between two successive poll() calls.
     """
 
+    def __init__(self, sensor_service, config=None, poll_interval=5):
+        super(NapalmBGPSensor, self).__init__(sensor_service=sensor_service,
+                                              config=config,
+                                              poll_interval=poll_interval)
+        self._logger = self.sensor_service.get_logger(
+            name=self.__class__.__name__
+        )
+
+        # self._poll_interval = 30
+
     def setup(self):
         # Setup stuff goes here. For example, you might establish connections
         # to external system once and reuse it. This is called only once by the system.
 
         # TODO Figure out how to use this and make a config.yaml
         #
-        
+
         # database = database or self.config.get('default')
         # db_config = self.config.get(database, {})
         # params = {
@@ -35,13 +47,6 @@ class NapalmBGPSensor(PollingSensor):
         #     'password': password or db_config.get('password')
         # }
 
-        # Use the appropriate network driver to connect to the device:
-        driver = napalm.get_network_driver('junos')
-
-        # Connect:
-        device = driver(hostname='10.102.128.168', username='root',
-                        password='Juniper', optional_args={'port': 2222})
-
         # Need to get initial BGP RIB, neighbor table, etc. Put into "self".
         # Then, write some logic within "poll" that checks again, and detects diffs
         # Detects:
@@ -49,10 +54,52 @@ class NapalmBGPSensor(PollingSensor):
         #   (may want to not only store the previous result, but also the previous 10 or so and do a stddev calc)
 
         # Stores number of BGP neighbors
-        bgp_neighbors = 0
+        # self.bgp_neighbors = 0
 
         # Stores RIB information in-between calls
-        bgp_rib = {}
+        # self.bgp_rib = {}
+
+        # Dictionary for tracking per-device known state
+        # Top-level keys are the management IPs sent to NAPALM, and
+        # information on each is contained below that
+        self.device_state = {}
+
+        napalm_config = self._config
+
+        # Assign sane defaults for configuration
+        default_opts = {
+            "opt1": "val1"
+        }
+        for opt_name, opt_val in default_opts.items():
+
+            try:
+
+                # key exists but is set to nothing
+                if not napalm_config[opt_name]:
+                    napalm_config[opt_name] == default_opts
+
+            except KeyError:
+
+                # key doesn't exist
+                napalm_config[opt_name] == default_opts
+
+        # Assign options to instance
+        self._devices = napalm_config['devices']
+
+        # Generate dictionary of device objects per configuration
+        # IP Address(key): Device Object(value)
+        #
+        # TODO(mierdin): Yes I know this looks terrible, I will fix it
+        self.devices = {
+            str(device['host']): get_network_driver(device['driver'])(
+                hostname=str(device['host']),
+                username=device['username'],
+                password=device['password'],
+                optional_args={
+                    'port': str(device['port'])
+                })
+            for device in self._devices
+        }
 
     def poll(self):
         # This is where the crux of the sensor work goes.
@@ -73,52 +120,60 @@ class NapalmBGPSensor(PollingSensor):
         #   # trace_tag is a tag you would like to associate with the dispacthed TriggerInstance
         #   # Typically the trace_tag is unique and a reference to an external event.
 
-        # Use the appropriate network driver to connect to the device:
-        driver = get_network_driver('junos')
+        for hostname, device_obj in self.devices.items():
 
-        ip_addresses = ["172.28.128.10"]
+            try:
+                last_bgp_peers = self.device_state[hostname]["last_bgp_peers"]
+            except KeyError:
 
-        devices = [driver(hostname=address, username='root',
-                          password='Juniper', optional_args={'port': 22})
-                   for address in ip_addresses]
+                # Get current BGP peers (instead of setting to 0 and
+                # triggering every time sensor starts initially)
+                try:
+                    self.device_state[hostname] = {
+                        "last_bgp_peers": self.get_number_of_peers(device_obj)
+                    }
+                    continue
+                # Any connection-related exception raised here is
+                # driver-specific, so we have to catch "Exception"
+                except Exception as e:
+                    self._logger.debug("Caught exception on connect: %s" % e)
+                    continue
 
-        this_bgp_neighbors = 0
+            try:
+                this_bgp_peers = self.get_number_of_peers(device_obj)
+            # Any connection-related exception raised here is
+            # driver-specific, so we have to catch "Exception"
+            except Exception as e:
+                self._logger.debug("Caught exception on get peers: %s" % e)
+                continue
 
+            if this_bgp_peers > last_bgp_peers:
+                self._logger.info(
+                    "Peer count went UP to %s" % str(this_bgp_peers)
+                )
+                self._bgp_peer_trigger(BGP_PEER_INCREASE, hostname,
+                                       last_bgp_peers, this_bgp_peers)
 
-        for device in devices:
+            elif this_bgp_peers < last_bgp_peers:
+                self._logger.info(
+                    "BGP neighbors went DOWN to %s" % str(this_bgp_peers)
+                )
 
-            # Retrieve full BGP peer info at the VRF level
-            with device:
-                vrfs = device.get_bgp_neighbors()
+                self._bgp_peer_trigger(BGP_PEER_DECREASE, hostname,
+                                       last_bgp_peers, this_bgp_peers)
 
-            vrfs = {}
+            elif this_bgp_peers == last_bgp_peers:
+                self._logger.info(
+                    "BGP neighbors STAYED at %s" % str(this_bgp_peers)
+                )
 
-            # import pdb; pdb.set_trace()
-            for vrf_id, vrf_details in vrfs.items():
-
-                for peer_id, peer_details in vrf_details['peers'].items():
-
-                    import pdb; pdb.set_trace()
-
-                    # TODO(mierdin): This isn't always the fastest method when
-                    # peers go down. Try to improve on this.
-                    if peer_details['is_up']:
-                        this_bgp_neighbors += 1
-
-
-        if this_bgp_neighbors > bgp_neighbors:
-            print "BGP neighbors went UP to %s" % str(this_bgp_neighbors)
-        elif this_bgp_neighbors == bgp_neighbors:
-            print "BGP neighbors STAYED at %s" % str(this_bgp_neighbors)
-        elif this_bgp_neighbors < bgp_neighbors:
-            print "BGP neighbors went DOWN to %s" % str(this_bgp_neighbors)
-    
-
-    bgp_neighbors = this_bgp_neighbors
+            # Save this state for the next poll
+            self.device_state[hostname]["last_bgp_peers"] = this_bgp_peers
 
     def cleanup(self):
-        # This is called when the st2 system goes down. You can perform cleanup operations like
-        # closing the connections to external system here.
+        # This is called when the st2 system goes down. You can perform
+        # cleanup operations like closing the connections to external
+        # system here.
         pass
 
     def add_trigger(self, trigger):
@@ -132,3 +187,38 @@ class NapalmBGPSensor(PollingSensor):
     def remove_trigger(self, trigger):
         # This method is called when trigger is deleted
         pass
+
+    def get_number_of_peers(self, device_obj):
+
+        vrfs = {}
+
+        # Retrieve full BGP peer info at the VRF level
+        try:
+            with device_obj:
+                vrfs = device_obj.get_bgp_neighbors()
+        except Exception:  # TODO(mierdin): convert to ConnectionException
+            raise
+
+        this_bgp_peers = 0
+
+        for vrf_id, vrf_details in vrfs.items():
+
+            for peer_id, peer_details in vrf_details['peers'].items():
+
+                # TODO(mierdin): This isn't always the fastest method when
+                # peers go down. Try to improve on this.
+                if peer_details['is_up']:
+                    this_bgp_peers += 1
+
+        return this_bgp_peers
+
+    def _bgp_peer_trigger(self, trigger, hostname, oldpeers, newpeers):
+        trigger = 'napalm.BgpPeerDecrease'
+        payload = {
+            'device': hostname,
+            'oldpeers': int(oldpeers),
+            'newpeers': int(newpeers),
+            'timestamp': str(datetime.now()),
+        }
+        self._logger.debug("DISPATCHING TRIGGER %s" % trigger)
+        self._sensor_service.dispatch(trigger=trigger, payload=payload)
